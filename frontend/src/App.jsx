106d4cbeeb64d788dc0ba7b7dec7470703385bab
ozmberkan <ozmberkan@gmail.com>
2024-11-22 12:39:18 +0300

    first commit nodejs project with aniltrr

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex-grow flex justify-center items-center font-inter">
+      <div className="flex flex-col ">
+        <h1 className=" bg-clip-text text-transparent bg-gradient-to-r from-[#202020] to-[#c33c] font-black text-[100px] leading-tight ">
+          Node JS.
+        </h1>
+        <span className="font-semibold animate-pulse">Eğitimi Başlıyor...</span>
+      </div>
+    </div>
+  );
+};
+
+export default App;
